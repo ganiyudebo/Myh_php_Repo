8ff6fc4907be0152dd2519d462d799b844115957
ganiyudebo <62584356+ganiyudebo@users.noreply.github.com>
2021-07-05 01:42:37 +0100

    Create header.php
    The header and footer styling

---
+++ SIgnUp_Login_Form/templates/header.php
@@ -0,0 +1,40 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE HTML>
+<html>
+
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Validation</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand-text{
+            color: green !important;
+            text-align: left !important;
+
+        }
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+            
+        }
+    </style>
+
+</head>
+    <body class="grey lighten-4"> 
+        <nav class="white z-depth-0">
+            <div class="container">
+                <a href="landingpage.php" class="brand-logo brand-text">Form Validation Exercise</a>
+                 <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li><a href="form.php" class="btn z-depth-0">Sign Up</a> </li>
+                    <li><a href="login.php" class="btn z-depth-0">Login</a> </li> 
+                </ul>
+            </div>
+        </nav>
+
+        
