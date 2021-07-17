23679edd257740b0bb7798cb19140c20be1aa2d1
ganiyudebo <62584356+ganiyudebo@users.noreply.github.com>
2021-07-17 21:05:01 +0100

    Add files via upload

---
+++ Create_todo_list/edittask.php
@@ -0,0 +1,131 @@
+<?php
+ $conn = mysqli_connect('localhost','Ganiyu','1234','todo_list');
+
+ if(!$conn){
+     echo "unable to onnected" . mysqli_connect_error();
+ }
+
+ if(isset($_GET['id'])){
+     $id = mysqli_real_escape_string($conn, $_GET['id']);
+     
+     $sql = "SELECT * FROM Tasks WHERE id=$id";
+     
+     $result = mysqli_query($conn, $sql);
+
+     $singletask = mysqli_fetch_assoc($result);
+
+
+
+
+
+ }
+
+    $todo = $singletask['task'];
+    $time = $singletask['start_task_at'];
+    $duedate = $singletask['finish_task_by'];
+    $errors = array("todo"=>"", "time"=>"", "duedate"=>"");
+
+ if (isset($_POST["save"])) {
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // check name
+        if(!empty($_POST["todo"])){
+            $todo = clean_data($_POST["todo"]);
+            if(!preg_match("/^([a-zA-Z-\s]+)(,\s*[a-zA-Z\s]*)*$/", $todo)){
+                $errors["todo"] = "Can only be comma separated text";
+            } else {
+                // echo htmlspecialchars($todo);
+            }
+        } else {
+            $errors["todo"] = "This field is compulsory";
+        }
+        // check email
+        if(!empty($_POST["time"])){
+            $time = clean_data($_POST["time"]);
+            // echo htmlspecialchars($time);
+        } else {
+            $errors["time"] = "Task time must be filled";
+        }
+        // check website
+        if(!empty($_POST["duedate"])){
+            $duedate = clean_data($_POST["duedate"]);
+            
+            // if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)){
+               // echo htmlspecialchars($duedate);
+        } else {
+            $errors["duedate"] = "A due date must be entered";
+        }
+        
+    }
+
+    if(!array_filter($errors)){
+        
+        
+        // $_SESSION["name"] = $name;
+        // $_SESSION["email"] = $email;
+        // $_SESSION["password"] = $password;
+        // if (isset($_SESSION["email"])){
+        // print_r($_SESSION);
+        // }
+            
+            $todo = mysqli_real_escape_string($conn, $todo);
+            $time = mysqli_real_escape_string($conn, $time);
+            $duedate = mysqli_real_escape_string($conn, $duedate);
+            $id = mysqli_real_escape_string($conn, $_POST['id_to_update']);
+
+
+            echo $todo;
+            echo $time;
+
+            // $sql = "INSERT INTO  Tasks(task,start_task_at,finish_task_by) VALUES ('$todo','$time','$duedate')";
+            $sqlupdate = "UPDATE Tasks SET task='$todo', start_task_at='$time', finish_task_by='$duedate' WHERE id=$id ";
+
+            // // save to db and check
+            if(mysqli_query($conn, $sqlupdate)){
+                echo "Update success";
+                header("location: tasktable.php");
+            } else {
+                echo "there is an error: " .mysqli_error(); 
+            }
+
+        
+    }
+
+    } // END OF isset()
+
+    function clean_data($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        
+        return $data;
+    }
+
+?>
+
+
+<?php include("todoheader.php"); ?>
+
+
+<div class="center"><h5>On this page you can modify your task </h5></div>
+<!-- <?php if(!$singletask){
+    echo "Invalid task. try again";
+} ?> -->
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" validate method="post" >
+    <br>
+    Edit todo name: <input type = "text" name="todo" placeholder="<?php echo $singletask['task'] ;?>" value="<?php echo $todo; ?>" ><br>
+    <div style="color:red"><?php echo $errors["todo"]; ?></div>
+    Edit time for the task: <input type="time" name="time" value="<?php echo $time;?>"><br>
+    <div style="color:red"><?php echo $errors["time"]; ?></div>
+    Change completion due date: <input type="date" name="duedate" min="<?php echo date("Y-m-d"); ?>" value="<?php echo $duedate;?>"><br>
+    <div style="color:red"><?php echo $errors["duedate"]; ?></div>
+    <input type="hidden" name="id_to_update" value="<?php echo $singletask['id'] ;?>" >
+    <div class="center">
+        <input type="submit" name="save" value="save" class="btn">
+    </div>
+    
+    
+  
+    
+</form>
+
---
+++ Create_todo_list/todoheader.php
@@ -0,0 +1,53 @@
+<?php
+    // session_start();
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Validation</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="fontsawesome/css/all.min.css">
+    <style type="text/css">
+        .brand-text{
+            color: green !important;
+            text-align: left !important;
+
+        }
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+            
+        }
+
+        .tasktable{
+            max-width: 750px;
+            margin: 15px auto;
+            padding: 15px;
+        }
+        th.taskwidth{
+            width: 55%;
+        }
+    </style>
+
+</head>
+    <body class="grey lighten-4"> 
+        <nav class="white z-depth-0">
+            <div class="container">
+                <!-- <a href="landingpage.php" class="brand-logo brand-text">Form Validation Exercise</a> -->
+                <ul id="nav-mobile" class="left hide-on-small-and-down">
+                    <li><a href="tasktable.php" class="btn z-depth-0">View Existing Tasks</a> </li>
+                </ul>
+                 <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li><a href="todoform.php" class="btn z-depth-0">Add A Task</a> </li>
+                </ul>
+            </div>
+        </nav>
+
+        
